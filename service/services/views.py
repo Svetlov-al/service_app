78da802afa92bd4d9a0235820c42770628810f31
Svetlov-al <s3t1qs@gmail.com>
2023-12-28 20:23:14 +0400

    build celery

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import F, Prefetch, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -8,14 +8,30 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
+    price_with_discount = (
+            F('service__full_price') -
+            F('service__full_price') *
+            F('plan__discount_percent') / 100.00
+    )
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
-        'service',
         Prefetch('client',
                  queryset=Client.objects.all().select_related(
                      'user',
                  ).only('company_name',
                         'user__email')
                  )
+    ).annotate(
+        price=price_with_discount
     )
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        respone = super().list(request, *args, **kwargs)
+
+        respone_data = {'result': respone.data,
+                        'total_amount': queryset.aggregate(total=Sum('price')).get('total')}
+        respone.data = respone_data
+
+        return respone
