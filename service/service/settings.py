4b83a7516aff91595c58eb5ff65714c838586dc3
Svetlov-al <s3t1qs@gmail.com>
2023-12-28 18:37:28 +0400

    first prefetch request

--- service/service/settings.py
+++ service/service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 
+    'rest_framework',
     'clients',
     'services'
 ]
@@ -137,3 +138,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
